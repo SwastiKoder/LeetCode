ca8541caf02b47d313d83aa2fc536c9f6ba076d4
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-03 16:53:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isPerfectSquare(int n) {
+        // int lo = 0;
+        // int hi = n;
+        // while (lo <= hi) {
+        //     int mid = lo + (hi - lo) / 2;
+        //     long long p = n;
+        //     long long q = mid;
+        //     if ((q * q) == p) {
+        //         return true;
+        //         break ;
+        //     } else if ((q * q) > p) {
+        //         hi = mid - 1;
+        //     } else
+        //         lo = mid + 1;
+        // }
+        // return false ;
+        int root = sqrt(n);
+        if ((root * root) == n)
+            return true;
+        else
+            return false;
+    }
+    bool judgeSquareSum(int c) {
+        int x = 0;
+        int y = c;
+        while (x <= y) {
+            if (isPerfectSquare(x) && isPerfectSquare(y)) {
+                return true;
+                break;
+            }
+
+            else if (!isPerfectSquare(y)) {
+                y = (int)sqrt(y) * (int)sqrt(y);
+                x = c - y;
+            } else {
+                y = ((int)sqrt(x) + 1) *
+                    ((int)sqrt(x) +
+                     1); // for the next big number than x we use +1
+                y = c - x;
+            }
+        }
+        return false;
+    }
+}
+
+;
