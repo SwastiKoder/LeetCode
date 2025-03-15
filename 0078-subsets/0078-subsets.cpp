06f4577656bc9c7d787cb2cd47f31e056dc0f892
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-15 22:45:57 +0530

    Time: 3 ms (32.99%), Space: 15.8 MB (11.01%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -6,9 +6,9 @@ public:
             finalans.push_back(ans);
             return;
         }
-        helper(nums, ans, finalans, idx + 1);
+        helper(nums, ans, finalans, idx + 1); // exclude current element
         ans.push_back(nums[idx]);
-        helper(nums, ans, finalans, idx + 1);
+        helper(nums, ans, finalans, idx + 1); // include current element
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int idx = 0;
