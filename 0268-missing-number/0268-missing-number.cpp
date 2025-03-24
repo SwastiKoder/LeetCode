278bc955c61acdadd09e1480c5d6c47ef66b4202
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-24 19:49:27 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        // int i = 0;
-        // while (i < n) {
-        //     int correctIdx = nums[i];
-        //     if (nums[i] == i || nums[i] == n)
-        //         i++;
-        //     else {
-        //         swap(nums[i], nums[correctIdx]);
-        //     }
-        // }
-        // for (int i = 0; i < n; i++) {
-        //     if (nums[i] != i)
-        //         return i; // because the arrayis sorted and we do not have the indexed number suppose arr[i] = 9 as n = 9 but i = 4 which supposed to be return 4
-        // }
-        // return n;
-        int actualSum = (n*(n+1))/2 ;
-        int sum = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            sum += nums[i] ;
+        int i = 0;
+        while (i < n) {
+            int correctIdx = nums[i];
+            if (nums[i] == i || nums[i] == n)
+                i++;
+            else {
+                swap(nums[i], nums[correctIdx]);
+            }
         }
-        return actualSum - sum ;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i)
+                return i; // because the arrayis sorted and we do not have the indexed number suppose arr[i] = 9 as n = 9 but i = 4 which supposed to be return 4
+        }
+        return n;
+        //BEST SOLUTION 
+        // int actualSum = (n*(n+1))/2 ;
+        // int sum = 0 ;
+        // for(int i = 0 ; i < n ; i++){
+        //     sum += nums[i] ;
+        // }
+        // return actualSum - sum ;
     }
 };
