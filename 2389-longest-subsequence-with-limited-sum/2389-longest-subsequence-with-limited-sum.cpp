fa247555c83fa7afc35243935bfd67806ccf6d8b
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-27 16:26:49 +0530

    Time: 6 ms (44.18%), Space: 18.1 MB (27.08%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int m = queries.size();
         vector<int> ans(m);
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // O(nlogn)
         vector<int> pre(n);
         pre[0] = nums[0];
         for (int i = 1; i < n; i++) {
@@ -12,10 +12,17 @@ public:
         }
         for (int i = 0; i < m; i++) {
             int length = 0;
-            for (int j = 0; j < n; j++) {
-                if (pre[j] <= queries[i]) {
-                    length++;
-                }
+            //binary search for better TC but we can use loop in loop also
+            int lo = 0;
+            int hi = n - 1;
+            int target = queries[i];
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (pre[mid] <= target) {
+                    length = mid + 1;
+                    lo = mid + 1;
+                } else
+                    hi = mid - 1;
             }
             ans[i] = length;
         }
