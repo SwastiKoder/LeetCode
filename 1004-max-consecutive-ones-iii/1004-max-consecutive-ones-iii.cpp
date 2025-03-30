5e0659275033ee58a642c5fb9a7a3fd58ae8618b
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-30 09:23:12 +0530

    Time: 0 ms (100%), Space: 59.3 MB (26.19%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int maxLen = INT_MIN, len = INT_MIN, flips = 0;
-        int i = 0, j = 0;
         int n = nums.size();
+        int i = 0, j = 0, flips = 0;
+        int maxLen = INT_MIN;
         while (j < n) {
             if (nums[j] == 1)
                 j++;
-            else { // nums[j] = 0
-                if (flips < k) { //flips ko increase karo
+            else { // nums[j] == 0
+                if (flips < k) {
+                   j++;
                     flips++;
-                    j++;
-                }
-                else{ //flips = k
-                    len = j-i ;
-                    maxLen = max(len,maxLen) ;
-                    //i ko age wale ek 0 se age le jao
-                    while(nums[i]==1) i++ ; //now i is at 0 but we need 1 after this 0 so again
-                    i++ ;
-                    j++ ; // ek window ban gaya hoga i ko lene ke waad baad but chhoti length wali hogi so we do not calculate length again
+                } else {
+                    maxLen = max(maxLen, j - i);
+                    while(nums[i] ==1) i++ ; //mow i is at zero
+                    i++ ; //i is after the first zero 
+                    j++ ; //j is already suprassed 2 zero and at a zero that's why the operation of i runs and  we have not to decrement flips aur i ko ek zero age laye kyunki j k times zero ko suprass karega 
                 }
             }
         }
-         len = j-i ;
-                    maxLen = max(len,maxLen) ;
-        return maxLen ;
+         maxLen = max(maxLen, j - i);
+         return maxLen ;
     }
 };
