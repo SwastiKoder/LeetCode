8dcc5e799c933273a3e1a60df1183a6921184cc8
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-03-31 18:05:56 +0530

    Time: 0 ms (100%), Space: 65.1 MB (65.96%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -4,21 +4,22 @@ public:
         if (k <= 1)
             return 0; // see constraints
         int count = 0, product = 1;
-        int n = nums.size() ;
+        int n = nums.size();
         int i = 0, j = 0;
         while (j < n) {
             product = product * nums[j];
-            while(product>=k){
-                count = count + (j-i) ;
-                product = product/nums[i] ;
-                i++ ;
+            while (product >= k) {
+
+                product = product / nums[i];
+                i++;
             }
-            j++ ;
+            count = count + (j-i+1) ;
+            j++;
         }
-        while(i<n){
-            count = count + (j-i) ;
-            i++ ;
-        }
-        return count ;
+        // while(i<n){
+        //     count = count + (j-i) ;
+        //     i++ ;
+        // }
+        return count;
     }
 };
