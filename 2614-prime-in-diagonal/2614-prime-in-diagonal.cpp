1d4b6e778122e28225509961239afba14c212330
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-04-09 15:20:51 +0530

    Time: 1154 ms (5.44%), Space: 39.8 MB (10.58%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -13,8 +13,8 @@ public:
         }
         return 1;
     }
-    int diagonalPrime(vector<vector<int>>& nums) {
-        int maxel = INT_MIN;
+     int diagonalPrime(vector<vector<int>>& nums) {
+         int maxel = 0;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < nums.size(); j++) {
                 if (i == j) {
