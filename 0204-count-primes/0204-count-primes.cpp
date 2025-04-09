140cc9562b4b999f62c3a79d7b65efaae9b4169b
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-04-10 00:02:18 +0530

    Time: 483 ms (8.24%), Space: 12.6 MB (90.91%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void fillSieve(vector<bool>& sieve) {
+        int n = sieve.size() - 1; // actual number is given n but we created n+1
+        for (int i = 2; i <= sqrt(n); i++) {
+            for (int j = i * 2; j <= n; j = j + i) {
+                sieve[j] = 0;
+            }
+        }
+    }
+    int countPrimes(int n) {
+        int count = 0;
+        vector<bool> sieve(n + 1, 1); // 1 means prime , 0 means composite
+        fillSieve(sieve);
+        sieve[0] = 0;
+        sieve[1] = 0;
+        for (int i = 1; i < sieve.size()-1 ; i++) {
+            if (sieve[i] == 1)
+                count++;
+        }
+        return count;
+    }
+};
