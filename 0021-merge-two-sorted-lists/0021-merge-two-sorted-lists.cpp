0355b61a5ea36d0d7ce4c8fb180024bd24574289
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-05-06 13:28:34 +0530

    Time: 0 ms (100%), Space: 19.5 MB (28.17%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -2,28 +2,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
         ListNode* list3 = new ListNode(-1);
-        ListNode* temp3 = list3;
-        while (temp1 != NULL && temp2 != NULL) {
-            if (temp1->val <= temp2->val) {
-                ListNode* temp = new ListNode(temp1->val);
-                temp3->next = temp;
-                temp3 = temp3->next ;
-                temp1 = temp1->next;
+        ListNode* temp = list3;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+                temp = temp->next ;
             } else {
-                ListNode* temp = new ListNode(temp2->val);
-                temp3->next = temp;
-                temp3 = temp3 ->next ;
-                temp2 = temp2->next;
+                temp->next = list2;
+                list2 = list2->next ;
+                temp = temp->next;
             }
         }
-        if (temp1 == NULL) {
-            temp3->next = temp2;
+        if (list1 == NULL) {
+            temp->next = list2;
         }
-        if (temp2 == NULL) {
-            temp3->next = temp1;
+        if (list2 == NULL) {
+            temp->next = list1;
         }
         list3 = list3->next;
         return list3;
