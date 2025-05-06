df0d5384bf2c4df28c68f2a6249249e86fc96819
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-05-06 13:32:54 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.68%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,17 +8,16 @@ public:
             if (list1->val <= list2->val) {
                 temp->next = list1;
                 list1 = list1->next;
-                temp = temp->next ;
+                temp = temp->next;
             } else {
                 temp->next = list2;
-                list2 = list2->next ;
+                list2 = list2->next;
                 temp = temp->next;
             }
         }
         if (list1 == NULL) {
             temp->next = list2;
-        }
-        if (list2 == NULL) {
+        } else {
             temp->next = list1;
         }
         list3 = list3->next;
