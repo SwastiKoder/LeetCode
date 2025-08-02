3fb50e7ac36c19af11e8e49a75b081ae8777e919
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-08-02 15:08:31 +0530

    Time: 0 ms (100%), Space: 15.1 MB (76.8%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int level(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        return 1 + max(level(root->left), level(root->right));
-    }
-    void nthLevel(TreeNode* root, int curr, int level, vector<int>& v) {
-        if (root == NULL)
-            return;
-        if (curr == level) {
-            v.push_back(root->val);
-            return;
-        }
-        nthLevel(root->left, curr + 1, level, v);
-        nthLevel(root->right, curr + 1, level, v);
-    }
-    void nthLevelrev(TreeNode* root, int curr, int level, vector<int>& v) {
-        if (root == NULL)
-            return;
-        if (curr == level) {
-            v.push_back(root->val);
-            return;
-        }
-        nthLevelrev(root->right, curr + 1, level, v);
-        nthLevelrev(root->left, curr + 1, level, v);
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        int n = level(root);
-        for (int i = 1; i <= n; i++) {
+        if (root == NULL)
+            return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        int count = 0;
+        while (q.size() > 0) {
+            int n = q.size();
             vector<int> v;
-            if (i % 2 != 0)
-                nthLevel(root, 1, i, v);
-            else
-                nthLevelrev(root, 1, i, v);
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if (temp->left != NULL)
+                    q.push(temp->left);
+                if (temp->right != NULL)
+                    q.push(temp->right);
+            }
+
+            if (count % 2 != 0)
+                reverse(v.begin(), v.end());
             ans.push_back(v);
+            count++;
         }
         return ans;
     }
