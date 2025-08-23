1f8aae61b298ce472ba19e2094fab853c1e1857a
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-08-24 01:06:06 +0530

    Time: 3 ms (91.7%), Space: 65.8 MB (48.27%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> v;
+    void inorder(TreeNode* root) {
+        if (root == NULL)
+            return;
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
+    TreeNode* helper(vector<int>& ans, int lo, int hi) {
+        if (lo > hi)
+            return NULL;
+        int mid = (lo + hi) / 2;
+        TreeNode* temp = new TreeNode(ans[mid]);
+        temp->left = helper(ans, lo, mid - 1);
+        temp->right = helper(ans, mid + 1, hi);
+        return temp;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        inorder(root);
+        return helper(v, 0, v.size() - 1);
+    }
+};
