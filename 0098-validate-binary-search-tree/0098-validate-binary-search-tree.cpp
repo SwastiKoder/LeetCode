793214048caa498913b96b787d4290ceba953d5e
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-08-29 15:33:48 +0530

    Time: 0 ms (100%), Space: 22.2 MB (8.87%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,22 +1,34 @@
+
 class Solution {
 public:
-    TreeNode* prev = NULL;
-    bool flag = true;
-    void inorder(TreeNode* root) {
-        if (root == NULL)
-            return;
-        inorder(root->left);
-        if (prev != NULL) {
-            if (prev->val >= root->val) {
-                flag = false;
-                return;
+    void helper(TreeNode* root, vector<int>& ans) {
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left != NULL) {
+                TreeNode* pred = curr->left;
+                while (pred->right != NULL & pred->right != curr)
+                    pred = pred->right;
+                if (pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else if (pred->right == curr) {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            } else {
+                ans.push_back(curr->val);
+                curr = curr->right;
             }
         }
-        prev = root;
-        inorder(root->right);
     }
     bool isValidBST(TreeNode* root) {
-        inorder(root);
-        return flag;
+        vector<int> ans;
+        helper(root, ans);
+        for (int i = 0; i < ans.size() - 1; i++) {
+            if (ans[i] >= ans[i + 1])
+                return false;
+        }
+        return true;
     }
 };
