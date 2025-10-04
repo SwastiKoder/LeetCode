203ef44933906842958119b2ca34bd01a1b81652
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-04 13:14:31 +0530

    Time: 11 ms (15.74%), Space: 15.1 MB (69.34%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,63 +1,36 @@
-// class Node {
-// public:
-//     int val;
-//     Node* next;
-//     Node* random;
-// };
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // step1 create a deep copy without random pointer
-        Node* dummy = new Node(-1);
-        Node* tempC = dummy;
-        Node* temp = head;
-        while (temp) {
-            Node* a = new Node(temp->val);
-            tempC->next = a;
-            tempC = tempC->next;
-            temp = temp->next;
-        }
-        Node* duplicate = dummy->next;
-        // step2 alternate connections
-        dummy = new Node(-1);
         Node* a = head;
-        Node* b = duplicate;
-        Node* tempD = dummy;
-        while (a) {
-            tempD->next = a;
-            a = a->next;
-            tempD = tempD->next;
-            tempD->next = b;
-            b = b->next;
-            tempD = tempD->next;
+        Node* b = new Node(-1);
+        Node* temp = a;
+        Node* d = b;
+        while (temp) {
+            Node* dummy = new Node(temp->val);
+            d->next = dummy;
+            temp = temp->next;
+            d = d->next;
         }
-        dummy = dummy->next;
-        // step3 assigning random connections
-        Node* t1 = dummy; // t1 will traverse in the original list
-        while (t1) {
-            Node* t2 = t1->next; // t2 will traverse in the duplicate list
-            if (t1->random)
-                t2->random = t1->random->next;
-            t1 = t1->next->next;
+        b = b->next;
+        unordered_map<Node*, Node*> m;
+        Node* temp1 = a;
+        Node* temp2 = b;
+        while (temp1 != NULL) {
+            m[temp1] = temp2;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
-        // step4 separating the list
-        Node* d1 = new Node(-1);
-        Node* d2 = new Node(-2);
-        t1 = d1;
-        Node* t4 = d2;
-        Node* t = head;
-        while (t) {
-            t1->next = t;
-            t = t->next;
-            t1 = t1->next;
-            t4->next = t;
-            t = t->next;
-            t4 = t4->next;
+        for (auto ele : m) {
+            Node* o = ele.first;
+            Node* d = ele.second;
+            if (o->random == NULL)
+                d->random = NULL;
+            else {
+                if (m.find(o->random) != m.end()) {
+                    d->random = m[o->random];
+                }
+            }
         }
-        t1->next = NULL;
-        t4->next = NULL;
-        d1 = d1->next;
-        d2 = d2->next;
-        return d2;
+        return b;
     }
 };
