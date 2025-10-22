5aff8f4e5ca6ec88ade1fe33f6094c1d179559ad
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-22 22:48:07 +0530

    Time: 774 ms (16.34%), Space: 195.7 MB (25.37%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -3,24 +3,27 @@ public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         int maxVal = *max_element(nums.begin(), nums.end()) + k;
         unordered_map<int, int> freq;
-        vector<int> diff(maxVal + 2, 0);
+        map<int, int> diff;
         for (int x : nums) {
             freq[x]++;
+            diff[x] += 0;
             int l = max(0, x - k);
             int r = min(maxVal, x + k);
 
             diff[l]++;
             diff[r + 1]--;
         }
-        for (int i = 1; i < diff.size(); i++) {
-            diff[i] += diff[i - 1];
-        }
+
         int res = 0;
-        for (int target = 0; target <= maxVal; target++) {
-            int targetFreq = freq[target];
-            int needConv = diff[target] - targetFreq;
+        int cumSum = 0;
+        for (auto it = diff.begin(); it != diff.end(); it++) {
+            int target = it->first;
+            it->second += cumSum;
+           int targetCount = freq[target];
+            int needConv = it->second - targetCount;
             int possible = min(needConv, numOperations);
-            res = max(res, possible + targetFreq);
+            res = max(res, possible + targetCount);
+            cumSum =it->second;
         }
         return res;
     }
