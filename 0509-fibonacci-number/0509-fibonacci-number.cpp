22fa214fb48f76971406e93e7f5682b90e678df3
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-24 19:03:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-2)+fib(n-1);
+      int a = 0 ;
+      int b = 1;
+      int ans = 0;
+      for(int i = 0 ; i<n ; i++){
+        int temp = a+b;
+        b=a;
+        a=temp;
+      }
+      return a;
     }
 };
