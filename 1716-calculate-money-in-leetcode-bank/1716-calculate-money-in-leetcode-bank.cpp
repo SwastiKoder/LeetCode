c261cfb11b909f83e03cacf265d7b0f3642a66b6
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-26 13:10:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13.55%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -5,11 +5,8 @@ public:
         int a = 28;
         int d = 7;
         int remDay = n % 7;
-        int money = 0;
-        if (full == 1)
-            money = (2 * a + (full - 1) * d) / 2;
-        else
-            money = (full / 2) * (2 * a + (full - 1) * d);
+        int b = a + d * (full - 1);
+        int money = full * (a + b) / 2;
         int z = full + 1;
         for (int i = 1; i <= remDay; i++) {
             money += z;
