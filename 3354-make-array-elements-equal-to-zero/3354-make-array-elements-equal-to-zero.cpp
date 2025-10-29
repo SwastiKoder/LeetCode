7e30ba9d0f374fa794b18f1e3e19bb4a091c5d14
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-29 23:39:08 +0530

    Time: 189 ms (9.05%), Space: 24.1 MB (16.67%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
+    bool check(vector<int>& nums, int count, int i, int dir) {
+        vector<int> copy = nums;
+        int idx = i;
+        while (idx >= 0 && idx < copy.size() && count > 0) {
+
+            if (copy[idx] > 0) {
+                copy[idx]--;
+                dir *= -1;
+
+                if (copy[idx] == 0)
+                    count--;
+            }
+            idx += dir;
+        }
+        return count == 0;
+    }
     int countValidSelections(vector<int>& nums) {
+        int res = 0;
         int n = nums.size();
-        vector<long long> prefix(n, 0), suffix(n, 0);
 
-        // Compute prefix sum
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++)
-            prefix[i] = prefix[i - 1] + nums[i];
-
-        // Compute suffix sum
-        suffix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-            suffix[i] = suffix[i + 1] + nums[i];
-
-        int ans = 0;
-
+        int count = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0)
-                continue;
-
-            long long leftSum = (i > 0) ? prefix[i - 1] : 0;
-            long long rightSum = (i < n - 1) ? suffix[i + 1] : 0;
-
-            if (leftSum == rightSum)
-                ans += 2; // both directions valid
-            else if (abs(leftSum - rightSum) == 1)
-                ans += 1; // only one direction valid
+                count++;
         }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
 
-        return ans;
+                if (check(nums, count, i, -1))
+                    res++;
+
+                if (check(nums, count, i, 1))
+                    res++;
+            }
+        }
+        return res;
     }
 };
