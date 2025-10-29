b9ea4e76052fccb7b90f2332c9aa67227b3a7f97
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-30 01:19:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (11.64%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-
-        int count = 0;
-        while (n > 0) {
-            count++;
-            n /= 2;
-        }
         int sum = 0;
-        int y = 0;
-        while (count > 0) {
-            sum += pow(2, y);
-            y++;
-            count--;
+        for (int i = 0; i < INT_MAX; i++) {
+            sum += pow(2, i);
+            if (sum >= n)
+                return sum ;
         }
-        return sum;
+        return -1;
     }
 };
