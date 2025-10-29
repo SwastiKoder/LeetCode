a7499f73a438f8a8cb3aab301d17dbd62bd12201
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-10-30 00:22:09 +0530

    Time: 0 ms (100%), Space: 22.1 MB (68.1%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int count, int i, int dir) {
-        vector<int> copy = nums;
-        int idx = i;
-        while (idx >= 0 && idx < copy.size() && count > 0) {
-
-            if (copy[idx] > 0) {
-                copy[idx]--;
-                dir *= -1;
-
-                if (copy[idx] == 0)
-                    count--;
-            }
-            idx += dir;
-        }
-        return count == 0;
-    }
     int countValidSelections(vector<int>& nums) {
-        int res = 0;
-        int n = nums.size();
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++) {
 
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0)
-                count++;
-        }
-        for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
-
-                if (check(nums, count, i, -1))
-                    res++;
-
-                if (check(nums, count, i, 1))
-                    res++;
+                int idx = i;
+                int lsum = 0;
+                int rsum = 0;
+                for (int j = 0; j < i; j++) {
+                    lsum += nums[j];
+                }
+                for (int j = i; j < nums.size(); j++) {
+                    rsum += nums[j];
+                }
+                if (lsum == rsum)
+                    result += 2;
+                if ((lsum - rsum) == 1 || (rsum - lsum) == 1)
+                    result++;
             }
         }
-        return res;
+        return result;
     }
 };
