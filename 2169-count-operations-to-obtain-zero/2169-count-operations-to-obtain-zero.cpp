569b483dbebba517500e3396853d1d4128abe594
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-11-09 14:14:42 +0530

    Time: 0 ms (100%), Space: 7.9 MB (40.22%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        if(num1 == 0 || num2 == 0) return 0 ;
-        if(num1 >= num2) return 1 + countOperations(num1 - num2 , num2) ;
-        else return 1 + countOperations(num1 , num2-num1) ;
+        int count = 0;
+        while (num1 > 0 && num2 > 0) {
+            count += (num1 / num2);
+            num1 = num1 % num2;
+            swap(num1, num2);
+        }
+        return count;
     }
 };
