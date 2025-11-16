fba731bfed0d0eae777e4cdbc47e4bf8f3cb5bbe
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-11-16 12:09:52 +0530

    Time: 6 ms (28.69%), Space: 11.2 MB (14.31%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        int x = 2;
-        if (n == 1)
-            return "1";
-        string ans = "1";
-
-        while (x <= n) {
-            string str = "";
-            int count = 1;
-            for (int i = 1; i <= ans.length(); i++) {
-                if (i < ans.length() && ans[i] == ans[i - 1]) {
-                    count++;
-                } else {
-                    str += to_string(count);
-                    str.push_back(ans[i - 1]);
-                    count = 1;
-                    
-                }
+    string speak(string prev) {
+        int count = 1;
+        string ans = "";
+        for (int i = 1; i <= prev.size(); i++) {
+            if (i < prev.size() && prev[i] == prev[i - 1]) {
+                count++;
+            } else {
+                ans += to_string(count);
+                ans += prev[i - 1];
+                count = 1;
             }
-            ans = str;
-            x++;
         }
         return ans;
+    }
+    string countAndSay(int n) {
+        if (n == 1)
+            return "1";
+        string prev = countAndSay(n - 1);
+        return speak(prev);
     }
 };
