faac3d5c95a8b62320cb2b1e2adacea0dc828388
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-11-22 20:23:51 +0530

    Time: 51 ms (40.91%), Space: 113.2 MB (57.23%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,36 @@
+class RandomizedSet {
+public:
+    vector<int> v;
+    unordered_map<int, int> mp;
+    
+    RandomizedSet() {  }
+
+    bool insert(int val) {
+        if (mp.find(val) == mp.end()) {
+            v.push_back(val);
+            
+            mp.insert({val, v.size() - 1});
+            return true;
+        }
+        return false;
+    }
+
+    bool remove(int val) {
+        if (mp.find(val) != mp.end()) {
+            mp[v.back() ] = mp[val];
+            swap(v.back() , v[mp[val]]);
+
+            v.pop_back();
+            
+            mp.erase(val);
+
+            return true;
+        }
+        return false;
+    }
+
+    int getRandom() {
+        int idx = rand() % v.size();
+        return v[idx];
+    }
+};
