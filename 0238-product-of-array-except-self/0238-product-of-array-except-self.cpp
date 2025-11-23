4b9905537bc284561fc45d58d57c68c044e3203b
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-11-23 14:16:32 +0530

    Time: 0 ms (100%), Space: 42.2 MB (25.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pre(
-            n); // prefix product array excluding the number at that index
-        //  vector<int> suf(n); // suffix product array excluding the number at
-        //  that index
+        vector<int> pre(n);
+        vector<int> suf(n);
         int p1 = 1;
         pre[0] = p1;
         for (int i = 1; i < n; i++) {
@@ -13,17 +11,14 @@ public:
             pre[i] = p1;
         }
         p1 = 1;
-        // suf[n - 1] = p1;
+        suf[n - 1] = p1;
         for (int i = n - 2; i >= 0; i--) {
             p1 = p1 * nums[i + 1];
-            pre[i] = pre[i] * p1; // my thinking process was that the last or
-                                  // first indexed element of ans
-                                  // array is same as prefix and suffix array
-                                  // because they are only multiplied by 1 only
+            suf[i] = p1 ;
         }
-        // for (int i = 0; i < n; i++) {
-        //     nums[i] = pre[i] * suf[i];
-        // }
-        return pre;
+        for (int i = 0; i < n; i++) {
+            nums[i] = pre[i] * suf[i];
+        }
+        return nums;
     }
 };
