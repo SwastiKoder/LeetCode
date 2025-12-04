1d791f3e9786ce565e92daaeb9eadae3b960a0bd
SwastiKoder <swastikpriyadarshan925@gmail.com>
2025-12-04 20:18:56 +0530

    Time: 0 ms (100%), Space: 9.7 MB (6%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
         int count = 0;
-        int i;
-        for (i = n - 1; i >= 0;) {
-            if (s[i] == ' ')
-                i--;
-            else
-                break;
+        stringstream ss(s);
+
+        string word;
+
+        while (ss >> word) {
+            count = word.length();
         }
-        for (; i >= 0;) {
-            if (s[i] == ' ')
-                break;
-            else {
-                count++;
-                i--;
-            }
-        }
+
         return count;
     }
 };
