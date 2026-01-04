9173f25d365024cad24e510cc221785e8553c192
SwastiKoder <swastikpriyadarshan925@gmail.com>
2026-01-04 19:21:34 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.06%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
         int m = matrix.size();
         int n = matrix[0].size();
-        int minr = 0, minc = 0;
-        int maxr = m - 1, maxc = n - 1;
-        vector<int> v;
-        int tne = m * n;
+
+        int maxr = m - 1;
+        int maxc = n - 1;
+
+        int minr = 0;
+        int minc = 0;
+
         int count = 0;
+        int totalElements = m * n;
+
         while (minr <= maxr && minc <= maxc) {
-            // right
-            for (int j = minc; j <= maxc && count < tne; j++) {
-                v.push_back(matrix[minr][j]);
+            for (int j = minc; j <= maxc; j++) {
+                if (count < totalElements)
+                    ans.push_back(matrix[minr][j]);
                 count++;
             }
             minr++;
 
-            // down
-            for (int i = minr; i <= maxr && count < tne; i++) {
-                v.push_back(matrix[i][maxc]);
+            for (int i = minr; i <= maxr; i++) {
+                if (count < totalElements)
+                    ans.push_back(matrix[i][maxc]);
                 count++;
             }
             maxc--;
-            // left
-            for (int j = maxc; j >= minc && count < tne; j--) {
-                v.push_back(matrix[maxr][j]);
+
+            for (int j = maxc; j >= minc; j--) {
+                if (count < totalElements)
+                    ans.push_back(matrix[maxr][j]);
                 count++;
             }
             maxr--;
 
-            // up
-            for (int i = maxr; i >= minr && count < tne; i--) {
-                v.push_back(matrix[i][minc]);
+            for (int i = maxr; i >= minr; i--) {
+                if (count < totalElements)
+                    ans.push_back(matrix[i][minc]);
                 count++;
             }
             minc++;
         }
-        return v;
+        return ans;
     }
 };
