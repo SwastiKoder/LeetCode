8b5a681244753931b0f543052e356203a6ce6c59
SwastiKoder <swastikpriyadarshan925@gmail.com>
2026-01-12 11:32:10 +0530

    Time: 0 ms (100%), Space: 11.9 MB (26.83%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if (ransomNote.length() > magazine.length())
-            return false;
+        // // if (ransomNote.length() > magazine.length())
+        //     return false;
 
         unordered_map<char, int> mp;
         for (auto x : magazine) {
