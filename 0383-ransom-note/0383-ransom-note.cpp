da3e42398275365a203c17e681c728292dfa9a79
SwastiKoder <swastikpriyadarshan925@gmail.com>
2026-01-12 11:31:50 +0530

    Time: 0 ms (100%), Space: 11.9 MB (26.83%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.length() > magazine.length())
+            return false;
+
+        unordered_map<char, int> mp;
+        for (auto x : magazine) {
+            mp[x]++;
+        }
+
+        int x = ransomNote.length();
+
+        for (int i = 0; i < x; i++) {
+            if (mp.find(ransomNote[i]) != mp.end()) {
+                mp[ransomNote[i]]--;
+                if (mp[ransomNote[i]] == 0)
+                    mp.erase(ransomNote[i]);
+            } else
+                return false;
+        }
+
+        return true;
+    }
+};
