0000e9627e2e38bcb35d9fda31fc5c19c9fba7c8
SwastiKoder <swastikpriyadarshan925@gmail.com>
2026-01-15 14:50:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> ans(m, -1);
+        unordered_map<int, int> mp;
 
-        // vector<int>result;
-        for (int i = 0; i < nums1.size(); i++) {
+        for (int i = 0; i < n; i++) {
+            mp[nums2[i]] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
             int idx = -1;
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums2[j] == nums1[i]) {
+            for (int j = mp[nums1[i]]; j < n; j++) {
+                if(nums2[j] > nums1[i]){
                     idx = j;
                     break;
                 }
             }
-            if (idx == -1)
-                nums1[i] = idx;
-            else {
-                if (idx == nums2.size() - 1) {
-                    nums1[i] = -1;
-                    continue;
-                }
-                for (int k = idx + 1; k < nums2.size(); k++) {
-                    if (nums2[k] > nums1[i]) {
-                        nums1[i] = nums2[k];
-                        break;
-                    } else
-                        nums1[i] = -1;
-                }
-            }
+            if(idx != -1) ans[i] = idx;
         }
-        return nums1;
+        return ans;
     }
 };
