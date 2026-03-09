638d82bf4b72353d0e6bd99b32634e3a15ce044a
SwastiKoder <swastikpriyadarshan925@gmail.com>
2026-03-09 10:29:10 +0530

    Time: 0 ms (100%), Space: 16.9 MB (99.83%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int>pq;
+    int lessEle(vector<vector<int>>& matrix, int val) {
+        int n = matrix.size();
+        int row = n - 1;
+        int col = 0;
+        int count = 0;
 
-        for(int i = 0 ; i < matrix.size() ; i++){
-            for(int j = 0 ; j < matrix.size() ; j++){
-                pq.push(matrix[i][j]);
-                if(pq.size() > k)pq.pop();
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] > val) {
+                row--;
+            } else {
+                count += (row + 1);
+                col++;
             }
         }
+        return count;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
 
-        return pq.top();
+        int lo = matrix[0][0];
+        int hi = matrix[n - 1][n - 1];
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            int rank = lessEle(matrix, mid);
+           
+             if (rank < k)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+
+        return lo;
     }
 };
